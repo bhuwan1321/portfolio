575e11e85161b30781dd20983466a37b97df9801
bhuwan1321 <23071a6954@vnrvjiet.in>
2025-06-27 18:17:42 +0530

    layout

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Hero from './components/Hero.jsx'
 import Footer from './components/Footer.jsx'
+import Test from './components/test.jsx'
+import './gobal.css'
+import img from './components/IMG_1646[1].jpg';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Hero />
-    <Footer />
+     
+    <Hero/>
+    <Footer /> 
   </StrictMode>,
 )
